e794a1833bd1562f6291230ed2ab6451782c790e
drftgyhuy7njhbg <gs1b.mouser@hotmail.com>
2024-05-06 11:32:58 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-function firstMissingPositive(nums) {
-  const n = nums.length;
-  for (let i = 0; i < n; i++) {
-    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
-      const temp = nums[nums[i] - 1];
-      nums[nums[i] - 1] = nums[i];
-      nums[i] = temp;
-    }
-  }
-  for (let i = 0; i < n; i++) {
-    if (nums[i] !== i + 1) return i + 1;
-  }
-  return n + 1;
+function kthSmallest(matrix, k) {
+  const flatten = matrix.reduce((acc, row) => acc.concat(row), []);
+  flatten.sort((a, b) => a - b);
+  return flatten[k - 1];
 }
