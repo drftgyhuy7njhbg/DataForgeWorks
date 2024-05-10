e802bd6e69daff6310fda89e323348e59e111e76
drftgyhuy7njhbg <gs1b.mouser@hotmail.com>
2024-05-10 09:30:38 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const insertionSort = (arr) => {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
+function getIntersectionNode(headA, headB) {
+  let currA = headA;
+  let currB = headB;
+  while (currA !== currB) {
+    currA = currA ? currA.next : headB;
+    currB = currB ? currB.next : headA;
   }
-  return arr;
-};
+  return currA;
+}
