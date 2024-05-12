66a894f3a3443ffc0cf94c4d926ebb9e137455da
drftgyhuy7njhbg <gs1b.mouser@hotmail.com>
2024-05-12 17:32:37 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
-  }
-  return false;
-}
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size),
+  );
